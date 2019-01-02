cf328e0d93b48ba54a22f3544bde6a803195da1a
wattswang <15189839843@163.com>
2019-01-02 17:10:51 +0800

    初次提交

---
+++ fabric-service/src/main/java/com/asiainfo/fabric/service/G.java
@@ -0,0 +1,13 @@
+/*
+* Copyright @ 2019 com.asiainfo.com
+* fabric-service 下午3:36:33
+* All right reserved.
+*
+*/
+
+package com.asiainfo.fabric.service;
+
+public class G {
+
+}
+
---
+++ fabric-service/src/main/java/com/asiainfo/fabric/service/util/FabricConfig.java
@@ -0,0 +1,13 @@
+/*
+* Copyright @ 2019 com.asiainfo.com
+* fabric-service 下午3:44:17
+* All right reserved.
+*
+*/
+
+package com.asiainfo.fabric.service.util;
+
+public class FabricConfig {
+
+}
+
